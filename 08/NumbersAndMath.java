ced50f7541e4d76ca07f912f330ddb434af84491
kat323 <36419477+kat323@users.noreply.github.com>
2018-02-15 12:07:21 -0500

    update to solution 08
    Correction done on the assignment 08.

--- 08/NumbersAndMath.java
+++ 08/NumbersAndMath.java
@@ -4,30 +4,30 @@ public class NumbersAndMath {
     //writes and returns a string
     System.out.println( "I will now count my chickens:" );
     //returns a string with a number
-    System.out.println( "Hens " + ( 25 + 30 / 6 ) );
+    System.out.println( "Hens " + ( 25.00 + 30.00 / 6.00 ) );
     //returns a string with a number
-    System.out.println( "Roosters " + ( 100 - 25 * 3 % 4 ) );
+    System.out.println( "Roosters " + ( 100.00 - 25.00 * 3.00 % 4.00 ) );
     //returns a string
     System.out.println( "Now I will count the eggs:" );
     //returns a number
-    System.out.println( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 );
+    System.out.println( 3.00 + 2.00 + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 + 6.00 );
     //returns a string and a number
-    System.out.println( "Is it true that 3 + 2 < 5 - 7?" );
+    System.out.println( "Is it true that 3.00 + 2.00 < 5.00 - 7.00?" );
     //returns false
-    System.out.println( 3 + 2 < 5 - 7 );
+    System.out.println( 3.00 + 2.00 < 5.00 - 7.00 );
     //returns a string and a number
-    System.out.println( "What is 3 + 2? " + ( 3 + 2 ) );
+    System.out.println( "What is 3.00 + 2.00? " + ( 3.00 + 2.00 ) );
     //returns a string and a number
-    System.out.println( "What is 5 - 7? " + ( 5 - 7 ) );
+    System.out.println( "What is 5.00 - 7.00? " + ( 5.00 - 7.00 ) );
     //returns a string
     System.out.println( "Oh, that's why it's false." );
     //returns a string
     System.out.println( "How about some more." );
     //returns a string and "true"
-    System.out.println( "Is it greater? " + ( 5 > -2 ) );
+    System.out.println( "Is it greater? " + ( 5.00 > -2.00 ) );
     //returns a string and "true"
-    System.out.println( "Is it greater or equal? " + ( 5 >= -2 ) );
+    System.out.println( "Is it greater or equal? " + ( 5.00 >= -2.00 ) );
     //returns a string and "false"
-    System.out.println( "Is it less or equal? " + ( 5 <= -2 ) );
+    System.out.println( "Is it less or equal? " + ( 5.00 <= -2.00 ) );
     }
 }
